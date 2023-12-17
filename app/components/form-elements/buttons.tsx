a3427c211ebd9b76b02518985854be5f2308c051
Tejash Patel <pateltejash429@gmail.com>
2023-12-17 23:48:25 +0530

    Refactor login and register pages

--- app/components/form-elements/buttons.tsx
+++ app/components/form-elements/buttons.tsx
@@ -7,7 +7,7 @@ export default function GitHubButton({ i }: { i: number }) {
   return (
     <button className='bg-white btn hover:bg-slate-200 font-medium text-black'>
       {i == 1 ? <Github size={22} /> : <FcGoogle size={22} />}
-      {i == 1 ? 'Login with GitHub' : 'Login with Google'}
+      {i == 1 ? 'Continue with GitHub' : 'Continue with Google'}
     </button>
   );
 }
