96c97824784dbfa2f1e3918c5e6fe81e7c94b0a3
Shubham <patelshub168@gmail.com>
2024-01-12 11:27:15 +0530

    removed unwanted components

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-import Dashboard from './components/dashboard';
-import SideBar from './components/sideBar';
+
 
 export default function Home() {
   return (
     <main className='flex flex-wrap justify-between'>
-      <SideBar />
-      <Dashboard />
+      Hello 
     </main>
   );
 }
